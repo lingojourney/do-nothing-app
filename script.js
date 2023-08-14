cd78b1b403e8cc56b9e09666e98611fbf9dc55bf
lingojourney <lingojourney@users.noreply.github.com>
2023-08-14 12:00:45 +0100

    add remove button

--- script.js
+++ script.js
@@ -5,36 +5,47 @@ const tasksContainer = document.getElementById('tasks-container');
 
 // Add task
 addTaskBtn.addEventListener('click', () => {
-  const taskText = taskInput.value;
-  
-  // Create task div
-  const task = document.createElement('div');
-  task.classList.add('task');
-  
-  // Create task content
-  const content = document.createElement('p');
-  content.textContent = taskText;
-  task.appendChild(content);
-  
-  // Create do task button
-  const doTaskBtn = document.createElement('button');
-  doTaskBtn.textContent = 'Do Task';
-  doTaskBtn.addEventListener('click', () => {
-    alert(`Doing ${taskText}`);
-  });
-  task.appendChild(doTaskBtn);
-  
-  // Create do nothing button
-  const doNothingBtn = document.createElement('button');
-  doNothingBtn.textContent = 'Do Nothing';
-  doNothingBtn.addEventListener('click', () => {
-    alert('Doing nothing!'); 
-  });
-  task.appendChild(doNothingBtn);
-  
-  // Add task to container
-  tasksContainer.appendChild(task);
-  
-  // Clear input
-  taskInput.value = '';
+  const taskText = taskInput.value.trim();
+
+  // Check input is not empty
+  if (taskText) {
+    // Create task div
+    const task = document.createElement('div');
+    task.classList.add('task');
+
+    // Create task content
+    const content = document.createElement('p');
+    content.textContent = taskText;
+    task.appendChild(content);
+
+    // Create do task button
+    const doTaskBtn = document.createElement('button');
+    doTaskBtn.textContent = 'Do Task';
+    doTaskBtn.addEventListener('click', () => {
+      alert(`Doing ${taskText}`);
+    });
+    task.appendChild(doTaskBtn);
+
+    // Create do nothing button
+    const doNothingBtn = document.createElement('button');
+    doNothingBtn.textContent = 'Do Nothing';
+    doNothingBtn.addEventListener('click', () => {
+      alert('Doing nothing!');
+    });
+    task.appendChild(doNothingBtn);
+
+    // Create remove task button
+    const removeTaskBtn = document.createElement('button');
+    removeTaskBtn.textContent = 'Remove Task';
+    removeTaskBtn.addEventListener('click', () => {
+      tasksContainer.removeChild(task);
+    });
+    task.appendChild(removeTaskBtn);
+    
+    // Add task to container
+    tasksContainer.appendChild(task);
+    
+    // Clear input
+    taskInput.value = '';
+  }
 });
